123de275dd7a001b197177135a7fa7bcc8451e48
SBENG <buisness.sbeng@gmail.com>
2021-08-30 17:48:27 +0200

    update config

--- config/homebrew.sh
+++ config/homebrew.sh
@@ -1,3 +1,7 @@
-## Homebrew Path : BigSure+
-HBP=/opt/homebrew
-export PATH=$HBP/bin:$PATH
+if [[ "$IS_MACOS_GT_11" == true ]]; then
+	## Homebrew Path : BigSure+
+	HBP=/opt/homebrew
+	export PATH=$HBP/bin:$PATH
+else
+	export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+fi
---
+++ config/m1.sh
@@ -0,0 +1,3 @@
+if [[ "$IS_M1_SHIP" == true ]]; then
+	source $ALIAS_PATH/m1.sh
+fi
